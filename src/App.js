3c66b985f4ab060d6af49a2823b57bdcd8c2b4d4
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-07 22:34:07 +0900

    Handle Form submission via callbacks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
 import React from 'react';
-import UserInterface from './components/ui/UserInterface';
+import Form from './components/ui/Form';
 
-function App({props}) {
 
+/*
+We want our handleSubmit() function to ultimately help us create a new task, so we need 
+a way to pass information from <Form /> to <App />. We can't pass data from child to parent 
+in the same way as we pass data from parent to child using standard props. Instead, we can 
+write a function in <App /> that will expect some data from our form as an input, then pass 
+that function to <Form /> as a prop. This function-as-a-prop is called a callback prop. Once
+ we have our callback prop, we can call it inside <Form /> to send the right data to <App />
+ */
+function App() {
+  // 1. Create addTask function and pass addTask function into Form as a prop; callback prop
+  // 2. The name can be anything just chose addTask
+  // 3. Go to Form and use addTask as a prop in handleSubmit function
+  const addTask = name => {alert(name)}
   return (
     <div className='it'>
-      <UserInterface />
+      <Form addTask={addTask}/>
     </div>
   );
 }
--- src/components/ui/Form.js
+++ src/components/ui/Form.js
@@ -2,13 +2,19 @@ import React from "react";
 import FilterButtons from './Buttons/FilterButtons';
 import AddText from './AddText';
 import AddButtons from './Buttons/AddButtons';
-import TaskList from '../ui/TaskList';
+import TaskList from './TaskList';
 
-
-const UserInterface = ({ tasks }) => {
+// Create handleSubmit function
+// 1. Add onSubmit to and pass handleSubmit to form
+// 2. Add new addTask parameter and use addTask callback prop in handleSubmit
+const Form = ({ tasks, addTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask('Say Hello')
+  }
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 className='head'>What needs to be done?</h2>
         <AddText />
         <AddButtons/>
@@ -21,4 +27,4 @@ const UserInterface = ({ tasks }) => {
   );
 };
 
-export default UserInterface;
+export default Form;
